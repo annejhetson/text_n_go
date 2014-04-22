14808e4b634ee82be0c9fe2a1e886bdc8a65d2ee
Anne Jhetson & vincent rotter <student@epicodus.com>
2014-04-22 09:15:43 -0700

    fix homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def index
     @message = Message.new
-    @user = current_user
-    @recieved_messages = Message.where(to: @user.phone)
-    @all_users_not_self = User.all - [@user]
+    if !current_user.nil?
+      @user = current_user
+      @recieved_messages = Message.where(to: @user.phone)
+      @all_users_not_self = User.all - [@user]
+    end
   end
 end
