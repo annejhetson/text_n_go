36fe6904e1b4165098811a7c3efaab8e36cb399e
Anne Jhetson & vincent rotter <student@epicodus.com>
2014-04-22 17:30:30 -0700

    refactor send message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,25 +8,24 @@ class MessagesController < ApplicationController
   end
 
   def create
-    if !params[:user_phone].nil?
+    if params[:user_phone].nil? || params[:message][:body] == ""
+      @user= current_user
+      @message = Message.new
+      @messages = Message.all
+      flash[:alert] = "There was an error processing your message"
+      render 'users/index'
+    else
+      @array = []
+      @user = User.find(params[:user_id])
       params[:user_phone].each do |to|
         params[:message][:to] = to
-        @user = User.find(params[:user_id])
-        @message = @user.messages.new(message_params)
-        if @message.save
-          flash[:notice] = "Your message was sent!"
-        else
-          render '/users/index.html.erb'
-        end
+        @friend = Friend.find_by(phone: to)
+        params[:message][:friend_id] = @friend.id
+        params[:message][:user_id] = @user.id
+        @message = Message.create(message_params)
       end
+      flash[:notice] = ActionController::Base.helpers.pluralize(params[:user_phone].size, 'message') + " sent"
       redirect_to :back
-    else
-      @user = User.find(params[:user_id])
-      @all_users_not_self = User.all - [@user]
-      params[:message][:to] = params[:user_phone]
-      @message = @user.messages.new(message_params)
-      flash[:alert] = "try again!"
-      render '/users/index.html.erb'
     end
   end
 
@@ -37,6 +36,6 @@ class MessagesController < ApplicationController
 
 private
   def message_params
-    params.require(:message).permit(:to, :from, :body, :user_id)
+    params.require(:message).permit(:to, :from, :body, :user_id, :friend_id)
   end
 end
